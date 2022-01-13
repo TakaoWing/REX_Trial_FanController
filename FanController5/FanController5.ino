87286a102c2b22e5cf5fedbf39b52c0be01ad6da
Tsubasa Takaoka <tt.wing2838783@gmail.com>
2022-01-13 15:52:50 +0900

    Done:配列で強中弱プログラムの整理

--- FanController5/FanController5.ino
+++ FanController5/FanController5.ino
@@ -1,65 +1,46 @@
 /*扇風機を作ろう！その5
- * 内容
- * タッチセンサを押すたびにファン（モータ）のON/OFFが切り替わる
- * もう一つのタッチセンサでモータの強中弱を切り替える
- */
-const int powerSwitchPin = 13; // 扇風機の電源スイッチのピン
-const int changeSpeedSwitchPin = 12; // 扇風機の強さを変更する用のピン
+   内容
+   タッチセンサを押すたびにファン（モータ）のON/OFFが切り替わる
+   もう一つのタッチセンサでモータの強中弱を切り替える
+   配列を用いてプログラムを整理
+*/
+const int switchPins[] = {13, 12}; // スイッチのピンの配列
 const int motorPin = 9; // モータを動かすピン
-int powerSwitchState = 0; // 扇風機の電源スイッチの状態(State) 0:オフ，1:オン
-int old_powerSwitchState = 0; // 一回前の扇風機の電源スイッチの状態
-int changeSpeedSwitchState = 0; // 扇風機の強さを変更する用のスイッチの状態(State) 0:オフ，1:オン
-int old_changeSpeedSwitchState = 0; // 一回前の扇風機の強さを変更する用のスイッチの状態
-bool isRunning = false; // モーターを動かすかどうか false:動かさない, true:動かす
-int motorSpeed = 0;
-int fanMode = 0; // 0:弱, 1:中, 2:強
+int switchState = 0; // スイッチの今の状態
+int old_switchStates[] = {0, 0}; // 一回前のスイッチの状態
+bool isRunning = false; // モータを動かすかどうか false:動かさない, true:動かす
+int motorSpeeds[] = {60, 120, 254}; // モータスピードを格納
+int fanMode = 0;
 
 // Arduinoが起動した時，1度だけ実行
 void setup() {
- pinMode(powerSwitchPin,INPUT_PULLUP); // 扇風機の電源スイッチピンをinput pullupにする
- pinMode(changeSpeedSwitchPin, INPUT_PULLUP); // 扇風機の強さを変更する用のスイッチピンをinput pullupにする
- pinMode(motorPin,OUTPUT); // モータピンをoutputにする
- Serial.begin(9800);
+  for (int i = 0; i < 2; i++) { // 2回繰り返す
+    pinMode(switchPins[i], INPUT_PULLUP);
+  }
+  pinMode(motorPin, OUTPUT); // モータピンをoutputにする
 }
 
 // 起動中ずっと実行
 void loop() {
-
-  // 電源用のスイッチの設定
-  powerSwitchState = digitalRead(powerSwitchPin); // powerSwitchPinの状態をpowerSwitchStateに代入 押している時:LOW 離している時:HIGH
-  if(powerSwitchState == LOW && old_powerSwitchState == HIGH){ // もし「今の状態が押している時かつ前の状態が押していない時(スイッチを押した一瞬)」なら
-    isRunning = !isRunning; // 動きを切り替える
-  }
-
-  // モータの強弱ボタンの設定
-  changeSpeedSwitchState = digitalRead(changeSpeedSwitchPin);
-  if( changeSpeedSwitchState == LOW && old_changeSpeedSwitchState == HIGH){ // もし「今の状態が押している時かつ前の状態が押していない時(スイッチを押した一瞬)」なら
-    fanMode++;
-    if(fanMode>2){
-      fanMode = 0;
+  for (int i = 0; i < 2; i++) {
+    switchState =  digitalRead(switchPins[i]); // 今の状態を取得
+    if (switchState == LOW && old_switchStates[i] == HIGH) {
+      if (i == 0) { // 電源用ピンの場合
+        isRunning = !isRunning; // 動きを切り替える
+      } else if (i == 1) { // ファンの強さ制御ピンの場合
+        fanMode++;
+        if (fanMode > 2) fanMode = 0; // fanModeが3の場合，0に切り替える
+      }
     }
+    old_switchStates[i] = switchState; // 前の情報に今の情報を上書き
   }
 
-  //モータのモードによるモータスピードの設定
-  if(fanMode == 0){ // 弱
-    motorSpeed = 60;
-  }else if(fanMode == 1){ // 中
-    motorSpeed = 120;
-  }else if(fanMode == 2){ // 強
-    motorSpeed = 254;
+  if (isRunning) { // もし動かすなら
+    analogWrite(motorPin, motorSpeeds[fanMode]); // モータに電流を流す
+  }
+  else { // 動かさなければ
+    analogWrite(motorPin, 0); // モータに電流を流さない
   }
 
-  
-  if(isRunning){ // もし動かすなら
-    analogWrite(motorPin,motorSpeed); // モータに電流を流す
-  }
-  else{ // 動かさなければ
-    analogWrite(motorPin,0); // モータに電流を流さない
-  }
-  
-  // 今回のスイッチの状態をそれぞれに代入
-  old_powerSwitchState = powerSwitchState; 
-  old_changeSpeedSwitchState = changeSpeedSwitchState;
-  
   delay(100); //動作安定用
 }
