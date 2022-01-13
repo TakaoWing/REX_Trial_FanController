db5eb90ab094aa208e8a3a3fa02dccd39d08ca09
Tsubasa Takaoka <tt.wing2838783@gmail.com>
2022-01-13 12:23:05 +0900

    Initial commit

---
+++ FanController/FanController.ino
@@ -0,0 +1,14 @@
+/*扇風機を作ろう！その１
+ * 内容
+ * タッチセンサを押すと，ファン（モータ）が動くようにする
+ */
+const int switchPin = 13; // 13ピンをボタンにする
+const int moterPin = 9; // 
+int switchSate = 0;
+void setup() {
+
+}
+
+void loop() {
+
+}
