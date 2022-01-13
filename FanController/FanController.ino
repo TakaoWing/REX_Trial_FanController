49fbc658b57a2fbd4e6cf7d69f7118c7444dcb06
Tsubasa Takaoka <tt.wing2838783@gmail.com>
2022-01-13 12:33:58 +0900

    Done:タッチセンサを押している間，ファンが動くようにする

--- FanController/FanController.ino
+++ FanController/FanController.ino
@@ -1,14 +1,24 @@
 /*扇風機を作ろう！その１
  * 内容
- * タッチセンサを押すと，ファン（モータ）が動くようにする
+ * タッチセンサを押している間，ファン（モータ）が動くようにする
  */
-const int switchPin = 13; // 13ピンをボタンにする
-const int moterPin = 9; // 
-int switchSate = 0;
-void setup() {
+const int switchPin = 13; // タクトスイッチのピン
+const int motorPin = 9; // モータを動かすピン
+int switchState = 0; // タクトスイッチの状態(State) 0:オフ，1:オン
 
+// Arduinoが起動した時，1度だけ実行
+void setup() {
+ pinMode(switchPin,INPUT_PULLUP); // スイッチピンをinput pullupにする
+ pinMode(motorPin,OUTPUT); // モータピンをoutputにする
 }
 
+// 起動中ずっと実行
 void loop() {
-
+  switchState = digitalRead(switchPin); // switchPinの状態をswitchStateに代入 押している時:LOW 離している時:HIGH
+  if(switchState == LOW){ // もしスイッチが押されたら
+    digitalWrite(motorPin,HIGH); // モータに電流を流す
+  }
+  else{ // 押していない時
+    digitalWrite(motorPin,LOW); // モータに電流を流さない
+  }
 }
