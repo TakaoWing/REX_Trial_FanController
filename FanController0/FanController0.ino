4554a19825ef7583e9db82ca9272eabfe9958317
Tsubasa Takaoka <tt.wing2838783@gmail.com>
2022-03-10 13:23:01 +0900

    Create FanController0

---
+++ FanController0/FanController0.ino
@@ -0,0 +1,15 @@
+/*扇風機を作ろう！その0
+ * 内容
+ * モーターを回転させる
+ */
+const int motorPin = 9; // モータを動かすピン
+
+// Arduinoが起動した時，1度だけ実行
+void setup() {
+ pinMode(motorPin,OUTPUT); // モータピンをoutputにする
+}
+
+// 起動中ずっと実行
+void loop() {
+    digitalWrite(motorPin,HIGH); // モータに電流を流す
+}
